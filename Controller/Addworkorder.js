41dec047876a51821d200aa3c0f77635adce7cd9
suraj <surajshelkeofficial@gmail.com>
2023-03-18 08:30:43 +0530

    file upload

--- Controller/Addworkorder.js
+++ Controller/Addworkorder.js
@@ -6,20 +6,21 @@ const Addworkorder = db.Addworkorder;
 
 // Get All Product
 exports.addWorkorder = catchAsyncErrors(async (req, res, next) => {
-    const {WorkorderID,Title,AssignTo,File } = req.body;
+  let File = (req.body.File) ? req.body.File.name : null;
+    const {WorkorderID,Title,AssignTo} = req.body;
     // if (!WorkorderID || !Title || !AssignTo|| !File) {
     //   return next(new ErrorHander("Field is empty", 404));
     // }
-    const workorderIDexist = await Addworkorder.findOne({ where: { WorkorderID:WorkorderID  } });
-    if (workorderIDexist) {
-      return next(new ErrorHander("WorkorderID already exists", 404));
-    }
-    const Workorderdata = await Addworkorder.create({
-      WorkorderID: WorkorderID,
-      Title: Title,
-      AssignTo: AssignTo,
-      File:File
-    })
+    // const workorderIDexist = await Addworkorder.findOne({ where: { WorkorderID:WorkorderID  } });
+    // if (workorderIDexist) {
+    //   return next(new ErrorHander("WorkorderID already exists", 404));
+    // }
+    // const Workorderdata = await Addworkorder.create({
+    //   WorkorderID: WorkorderID,
+    //   Title: Title,
+    //   AssignTo: AssignTo,
+    //   File:File
+    // })
     if (Workorderdata) {
       res.status(201).send({ WorkorderID,Title,AssignTo,File});
     } else {
--- Routers/Addworkorder.js
+++ Routers/Addworkorder.js
@@ -2,7 +2,17 @@ const express = require("express");
 const { addWorkorder } = require("../Controller/Addworkorder");
 const router  = express.Router();
 
-router.route("/workorder/addworkorder").post(addWorkorder);
+const multer  = require('multer')
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'Uploads/workorderfiles/')
+    },
+    filename: function (req, file, cb) {
+      cb(null, Date.now() + '-' +  file.originalame )
+    }
+  })
+  const upload = multer({ storage:storage });
+router.route("/workorder/addworkorder",upload.single('File')).post(addWorkorder);
 // router.route("workorder/addworkorder").post(addWorkorder);
 
 module.exports = router;
